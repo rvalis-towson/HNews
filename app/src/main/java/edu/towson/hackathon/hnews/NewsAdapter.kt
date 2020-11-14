6e34d9615e7e7e148b50fc83ba310ce05bc254c0
Randy Valis <rvalis@towson.edu>
2020-11-13 19:26:42 -0500

    Step 3

--- app/src/main/java/edu/towson/hackathon/hnews/NewsAdapter.kt
+++ app/src/main/java/edu/towson/hackathon/hnews/NewsAdapter.kt
@@ -13,4 +13,25 @@ class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val position: TextView = view.position
 }
 
-// TODO - Part 3. Create the Adapter
+class NewsAdapter(private val activity: MainActivity) : RecyclerView.Adapter<NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item_view, parent, false)
+        val holder = NewsViewHolder(view)
+        view.article_url.setOnClickListener {
+            val position = holder.adapterPosition
+            activity.showArticle(position)
+        }
+        return holder
+    }
+
+    override fun getItemCount(): Int {
+        return activity.newsCount()
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val newsItem = activity.newsItem(position)
+        holder.title.text = newsItem.title
+        holder.url.text = newsItem.url
+        holder.position.text = (position + 1).toString()
+    }
+}
